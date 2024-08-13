3b09c30e3311da712cd7cb4cd6e288c11901b727
Nicolas VENTER <thedeal78@hotmail.fr>
2024-08-13 12:06:10 +0200

    Client + Server

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Webpack React Template
+# Webpack Preact Template
 
-This is a template for a really quick creation of React project with Webpack build.  
+This is a template for a really quick creation of Preact project with Webpack build.  
 This should be used with Visual Studio Code and the [Live Server extension](https://marketplace.visualstudio.com/items?itemName=ms-vscode.live-server).
 
 ## Installation
@@ -12,7 +12,10 @@ npm install -g -D webpack webpack-cli
 ```bash
 git clone https://github.com/nicolasventer/WebpackReactTemplate.git
 cd WebpackReactTemplate
-npm install
+cd Server
+bun install
+cd ../Client
+bun install
 ```
 
 ### Watch mode installation
@@ -23,24 +26,68 @@ npm install -g nodemon nodemon-webpack-plugin
 
 ## Demo
 
+*(Gif outdated)*
+
 ![Demo](./demo.gif)
 
 ## Usage
 
-Build: *(make a pre-check with tsc)*
+### Only Client (useful for hot reload that keeps the state)
+
+#### Development
+
+In the Client folder:
 
 ```bash
-npm run build
+bun run dev
 ```
 
-Rebuild: *(no pre-check, delete dist before build)*
+**WARNING:** Be sure to be exactly in the Client folder, otherwise you will have page not found error.
+
+#### Production
+
+In the Client folder:
 
 ```bash
-npm run rebuild
+bun run build
 ```
 
-Watch: *(no pre-check, no delete, watch for changes)*
+Then:
 
 ```bash
-npm run watch
+bun run preview
 ```
+
+### Server and Client
+
+#### Development
+
+In the Client folder:
+
+```bash
+bun run watch
+```
+
+Then open a new terminal and in the Server folder:
+
+```bash
+bun run dev
+```
+
+*Note: The client changes will be loaded on the refresh of the page.*
+
+#### Production
+
+In the Client folder:
+
+```bash
+bun run wbuild
+```
+
+Then open a new terminal and in the Server folder:
+
+```bash
+bun run start
+```
+
+*Note: You can rebuild the client at any time, the server will serve the new files.*
---
+++ Server/src/index.ts
@@ -0,0 +1,84 @@
+import { closeSync, fstatSync, openSync, readdirSync, watchFile } from "fs";
+import path from "path";
+import Watcher from "watcher";
+
+const CLIENT_PATH = path.join(__dirname, "../../Client");
+const DIST_PATH = path.join(CLIENT_PATH, "dist");
+const PAGE_PATH = path.join(CLIENT_PATH, "page.html");
+
+const distFiles = readdirSync(DIST_PATH, { withFileTypes: true, recursive: true }).map((file) =>
+	path.join(file.parentPath, file.name)
+);
+const pageFile = PAGE_PATH;
+
+const files = [pageFile, ...distFiles];
+
+const filesContent = await (async () => {
+	const result = {} as Record<string, string>;
+	await Promise.all(files.map(async (file) => (result[file] = await Bun.file(file).text())));
+	return result;
+})();
+
+const filesStatSize = await (async () => {
+	const result = {} as Record<string, number>;
+	await Promise.all(
+		files.map(async (file) => {
+			const path_fd = openSync(file, "r");
+			const path_stat = fstatSync(path_fd);
+			closeSync(path_fd);
+			result[file] = path_stat.size;
+		})
+	);
+	return result;
+})();
+
+const watcher = new Watcher(DIST_PATH, { recursive: true, native: true });
+
+const updateFile = (filename: string) => {
+	const oldSize = filesStatSize[filename];
+	const path_fd = openSync(filename, "r");
+	const path_stat = fstatSync(path_fd);
+	closeSync(path_fd);
+	const newSize = path_stat.size;
+	if (oldSize === newSize) return;
+	filesStatSize[filename] = newSize;
+	Bun.file(filename)
+		.text()
+		.then((content) => (filesContent[filename] = content));
+	console.log("File updated:", filename);
+};
+
+const deleteFile = (filename: string) => {
+	delete filesContent[filename];
+	delete filesStatSize[filename];
+	console.log("File deleted:", filename);
+};
+
+watcher.on("add", updateFile);
+watcher.on("change", updateFile);
+watcher.on("rename", deleteFile);
+watcher.on("unlink", deleteFile);
+
+watchFile(pageFile, (curr, prev) => {
+	if (curr.size === prev.size) return;
+	Bun.file(pageFile)
+		.text()
+		.then((content) => (filesContent[pageFile] = content));
+	console.log("Page file updated:", pageFile);
+});
+
+Bun.serve({
+	port: 3000,
+	fetch(req) {
+		const url = new URL(req.url);
+		const pathname = url.pathname === "/" ? "/page.html" : url.pathname;
+		const joinedPath = path.join(CLIENT_PATH, pathname);
+		console.log("joinedPath:", joinedPath);
+		if (files.includes(joinedPath)) {
+			return new Response(filesContent[joinedPath], { headers: { "Content-Type": "text/html" } });
+		}
+		return new Response("Nope");
+	},
+});
+
+console.log("Server started on http://localhost:3000");
